38d95ad9105f5100435afd2edbd44a726d17f28e
jgambi01 <jgambi01@student.bbk.ac.uk>
2026-08-13 23:38:23 +0100

    Add initial Food model and database migration

---
+++ CalorieTracker/Models/Food.cs
@@ -0,0 +1,12 @@
+﻿namespace CalorieTracker.Models
+{
+    public class Food
+    {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Calories { get; set; }
+            public decimal Protein { get; set; }
+            public decimal Carbohydrates { get; set; }
+            public decimal Fat { get; set; }
+        }
+    }
