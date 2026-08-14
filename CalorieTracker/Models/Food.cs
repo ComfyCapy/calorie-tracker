38fa6a139d0689bcf7939b1cff4c4b3fc55c900b
jgambi01 <jgambi01@student.bbk.ac.uk>
2026-08-14 22:17:26 +0100

    Add serving size & unit support

--- CalorieTracker/Models/Food.cs
+++ CalorieTracker/Models/Food.cs
@@ -1,12 +1,17 @@
-﻿namespace CalorieTracker.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace CalorieTracker.Models
 {
     public class Food
     {
-            public int Id { get; set; }
-            public string Name { get; set; } = string.Empty;
-            public int Calories { get; set; }
-            public decimal Protein { get; set; }
-            public decimal Carbohydrates { get; set; }
-            public decimal Fat { get; set; }
-        }
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Calories { get; set; }
+        public decimal Protein { get; set; }
+        public decimal Carbohydrates { get; set; }
+        public decimal Fat { get; set; }
+        [Display(Name = "Serving Size")]
+        public decimal ServingSize { get; set; } = 100;
+        [Display(Name = "Serving Unit")]
+        public string ServingUnit { get; set; } = "g";
     }
+}
